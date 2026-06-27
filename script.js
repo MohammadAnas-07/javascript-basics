a47f12d57d4e93998868fb310b5644de9c6c433c
Mohammad Anas <ansarianas53665@gmail.com>
2026-06-27 12:39:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+// Guess the number
+
+/* let number = 40
+let guess = 0
+
+do{
+     guess = parseInt( prompt("Guess the number")) 
+    if(guess == number){
+        alert('Winner')
+        break;
+        console.log(guessNo())    
+    }
+}while(guess != 0)
+ */
+
+
+// Function
+
+/* function sayHello(){
+    console.log("Anas")
+}
+sayHello() */
+
+
+//Parameter
+/* function add(num1,num2){
+    console.log(num1 + num2)
+}
+
+add(10,5) //parameter
+add(2,8)
+add(4,9) */
+
+/* function multiply(a,b){
+    let ans = a*b;
+    return ans;
+}
+const result = multiply(10,20)
+console.log("That hey user result is" , result) */
+
+
+//Unlimited Arguments accept
+
+/* function addNumbers(...numbers){ //spread operator
+    let ans = 0
+    for(let i = 0; i < numbers.length; i++){
+
+        ans = ans + numbers[i]
+    }
+    return ans;
+}
+let result = addNumbers(1,2,31,42,56)
+console.log(result)
+ */
+
+
+// '1.arguments' keyword
+/* const addNumbers1 = (...nums) =>{
+    console.log(nums)
+}
+addNumbers1(10, 22, 33, 44, 5)
+ */
+
+
+// 2.Hoisting
+//Normal Function
+sayHey()
+
+function sayHey(){
+    console.log("Heyy there")
+}
+
+//arrow function
+const sayHey1 = ()=>{
+    console.log('Hey there')
+}
+sayHey1()
+
+// 3. This Keyword
+
+const obj = {
+    vale: 20,
+    myFunction: function(){
+        console.log(this.vale)
+    }
+}
+obj.myFunction()
