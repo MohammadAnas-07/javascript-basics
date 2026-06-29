5c2b40a838ce028a07b2cc81ae09dc621598688a
Mohammad Anas <ansarianas53665@gmail.com>
2026-06-29 16:17:47 +0530

    async-await function

---
+++ Promise/script.js
@@ -0,0 +1,9 @@
+//Promise
+
+async function getData(){
+    let resultFromServer = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+    )
+    console.log(await resultFromServer.json())
+}
+getData()
